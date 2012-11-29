d593f2185f0207156da5daec7d65d80049082a91
Pete Graham <petegraham1@gmail.com>
2012-11-29 13:26:58 +0000

    files pulled back from phpwebhosting server

---
+++ wp-content/themes/Sharpfolio/footer.php
@@ -0,0 +1,9 @@
+
+<div id="footer">
+	<p>This Portfolio runs on <a href="http://webrevolutionary.com/sharpfolio/">Sharpfolio</a>, a WordPress Theme by <a href="http://webrevolutionary.com/">WebRevolutionary</a> and <a href="http://www.adimpact.com/">Adimpact</a>.</p>
+</div>
+</div>
+
+		<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/binary-news-10/footer.php
@@ -0,0 +1,5 @@
+<div class="footer">
+			<div class="padding">
+					 <a href="http://www.themeporter.com">Wordpress Themes</a> by ThemePorter | Design: <a href="http://www.free-css-templates.com">David Herreman </a> | Sponsored by <a href="http://www.webhostingbluebook.com">Cheap Web Hosting</a> |
+			</div>
+</div>
